9a15d4331a95662b60daaed36f0bce1ab7f4719e
lisnychyis <ljore@ukr.net>
2021-04-09 14:51:22 +0300

    update

---
+++ src/main/java/com/slisnychyi/travistest/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.slisnychyi.travistest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping
+    public String getName() {
+        return "my name";
+    }
+}
