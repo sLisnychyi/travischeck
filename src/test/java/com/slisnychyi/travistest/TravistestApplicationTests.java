469f3d2ecca8dea426398e71b0aa859863f20e5b
lisnychyis <ljore@ukr.net>
2021-04-09 14:39:24 +0300

    Initial commit

---
+++ src/test/java/com/slisnychyi/travistest/TravistestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slisnychyi.travistest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravistestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
